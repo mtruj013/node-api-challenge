9b956e48e686138f4990afcdd02ee1ad03bc13ff
Maria Trujillo <mptruj015@gmail.com>
2020-05-08 19:14:08 +0200

    finished all get and post requests for projects

--- data/helpers/projectRouter.js
+++ data/helpers/projectRouter.js
@@ -18,7 +18,6 @@ router.get('/', (req,res)=> {
     })
 });
 
-
 router.get('/:id', validateProjectId,(req, res) => {
     const id = req.params.id;
     
@@ -36,11 +35,43 @@ router.get('/:id', validateProjectId,(req, res) => {
 router.get('/:id/actions', validateProjectId, (req,res) => {
     const id = req.params.id;
 
+    Projects.getProjectActions(id)
+    .then(actions => {
+        res.status(200)
+        .json(actions)
+    })
+    .catch(error => {
+        res.status(500)
+        .json({error: "Could not find actions"})
+    })
+
   
 })
 
-router.post('/', (req,res)=> {
+router.post('/', validateProject, (req,res)=> {
+    Projects.insert(req.body)
+    .then(project => {
+        res.status(201)
+        .json(project)
+    })
+    .catch(error => {
+        res.status(500)
+        .json({error: "Could not add new project"})
+    })
+})
 
+router.post('/:id/actions', validateProjectId, validateAction,(req,res) => {
+    
+    Actions.insert(req.body)
+    .then(action => {
+        res.status(200)
+        .json(action)
+    })
+    .catch(error => {
+        res.status(500)
+        .json({error: "Unable to add action to project."})
+    })
+    
 })
 
 router.put('/:id', (req,res) => {
@@ -70,13 +101,33 @@ function validateProjectId(req,res,next){
 }
 
 function validateProject(req,res,next){
-    
+    const projectBody = req.body;
 
-
+    if(Object.keys(projectBody).length === 0){
+        res.status(400)
+        .json({message: "missing project data"})
+    } else if (!projectBody.name || !projectBody.description){
+        res.status(400)
+        .json({message: "Missing project name or description"})
+    } else {
+        next();
+    }
 }
 
 function validateAction(req,res,next){
 
+    if (!req.body.project_id){
+        res.status(400)
+        .json({error: "Missing the project ID"})
+    } else if(!req.body.description){
+        res.status(400)
+        .json({error: "Missing action description"})
+    } else if(!req.body.notes){
+        res.status(400)
+        .json({error: "Missing action notes"})
+    } else {
+        next();
+    }
 }
 
 module.exports = router;
